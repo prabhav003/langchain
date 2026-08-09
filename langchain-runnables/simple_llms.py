f41e4cc453a0b68f3bbdd0e54a1d0e296c3bafb3
Prabhav Singhal <prabhav0310@gmail.com>
2026-08-09 18:37:51 +0530

    updated

--- langchain-runnables/simple_llms.py
+++ langchain-runnables/simple_llms.py
@@ -1,17 +1,20 @@
-# from langchain.llms import OpenAI 
-# from langchain.prompts import PromptTemplate 
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
 
-# llm = OpenAI(model_name='gpt-3.5-turbo', temperature=0.7)
+llm = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0.7
+)
 
-# prompt = PromptTemplate(
-#     input_variables=['topic'],
-#     template='Suggest a catchy blog about {topic}'
-# )
+prompt = PromptTemplate(
+    input_variables=["topic"],
+    template="Suggest a catchy blog title about {topic}"
+)
 
-# topic = input('Enter a Topic')
+topic = input("Enter a topic: ")
 
-# formatted_prompt = prompt.format(topic=topic)
+formatted_prompt = prompt.format(topic=topic)
 
-# blog_title = llm.predict(formatted_prompt)
+response = llm.invoke(formatted_prompt)
 
-# print('Generated blog title:', blog_title)
+print("Generated blog title:", response.content)
